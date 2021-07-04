9b71a46b54ac02e891fd138383862ddf5f12353d
Davide Modolo <davide.modolo@studenti.unitn.it>
2021-07-04 15:45:59 +0200

    starting settings fragment

--- app/src/main/java/com/davidemodolo/insulyna/SettingsFragment.kt
+++ app/src/main/java/com/davidemodolo/insulyna/SettingsFragment.kt
@@ -1,23 +1,18 @@
-package com.davidemodolo.insulyna.ui.main
+package com.davidemodolo.insulyna
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.davidemodolo.insulyna.R
 
-class MainFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = MainFragment()
-    }
+class SettingsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 
 }
