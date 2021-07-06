e280a11dc23d64d4c2292bae9efc1288be03cb8a
Davide Modolo <davide.modolo@studenti.unitn.it>
2021-07-06 21:25:48 +0200

    created themes, working on settings

--- app/src/main/java/com/davidemodolo/insulyna/MainActivity.kt
+++ app/src/main/java/com/davidemodolo/insulyna/MainActivity.kt
@@ -1,18 +1,43 @@
 package com.davidemodolo.insulyna
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.ContentProviderCompat.requireContext
 import com.davidemodolo.insulyna.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private val PREF_NAME = "data"
+    private val THEME = "theme"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.nav_host_fragment, MainFragment.newInstance())
-                .commitNow()
+        switchTheme()
+    }
+
+    fun switchTheme()
+    {
+        val sharedPref: SharedPreferences = getSharedPreferences(PREF_NAME, 0)
+
+        when (sharedPref.getString(THEME, "pink")) {
+            "green" -> this.setTheme(R.style.ThemeGreen)
+            "pink" -> this.setTheme(R.style.ThemePink)
+            "yellow" -> this.setTheme(R.style.ThemeYellow)
+            "blue" -> this.setTheme(R.style.ThemeBlue)
+            "red" -> this.setTheme(R.style.ThemeRed)
+            "purple" -> this.setTheme(R.style.ThemePurple)
+            else -> { // Note the block
+                Toast.makeText(
+                    this,
+                    "Errore nel caricamento del tema personale",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
+
+
 }
