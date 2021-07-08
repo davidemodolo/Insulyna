b66fce384d036fb12623424a7ce2f5e05573efb6
Davide Modolo <davide.modolo@studenti.unitn.it>
2021-07-08 13:54:13 +0200

    missing food db, working on settings

--- app/src/main/java/com/davidemodolo/insulyna/SettingsFragment.kt
+++ app/src/main/java/com/davidemodolo/insulyna/SettingsFragment.kt
@@ -10,14 +10,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.navigation.fragment.findNavController
+import org.w3c.dom.Text
 
 class SettingsFragment : Fragment() {
     private val PREF_NAME = "data"
     private val THEME = "theme"
+    private val DAILY = "daily"
+    private val GOAL = "goal"
+    private val RATEO = "rateo"
+    private val SENS = "sens"
 
     private lateinit var themeDialog: Dialog
     override fun onCreateView(
@@ -29,11 +32,72 @@ class SettingsFragment : Fragment() {
         btnBack.setOnClickListener {
             findNavController().navigate(R.id.mainFragment)
         }
+        val sharedPref: SharedPreferences? = activity?.getSharedPreferences(PREF_NAME, 0)
+        var daily = sharedPref?.getString(DAILY, "")
+        var goal = sharedPref?.getString(GOAL, "")
+        var rateo = sharedPref?.getString(RATEO, "")
+        var sens = sharedPref?.getString(SENS, "")
+
+        val dailyLayout = view.findViewById<LinearLayout>(R.id.daily)
+        val dailyName = dailyLayout.findViewById<TextView>(R.id.preferenceName)
+        dailyName.text = getString(R.string.dailyUI_label)
+        val dailyValue = dailyLayout.findViewById<EditText>(R.id.preferenceValue)
+        dailyValue.setText(daily)
+        val dailyQuestion = dailyLayout.findViewById<ImageView>(R.id.preferenceQuestion)
+        dailyQuestion.setOnClickListener {
+            //funzione che apre un dialog globale della chat, passando un parametro per le cose da vedere
+            //bottone globale del dialog che salva il valore
+        }
+
+        val goalLayout = view.findViewById<LinearLayout>(R.id.goal)
+        val goalName = goalLayout.findViewById<TextView>(R.id.preferenceName)
+        goalName.text = getString(R.string.goal_label)
+        val goalValue = goalLayout.findViewById<EditText>(R.id.preferenceValue)
+        goalValue.setText(goal)
+        //val goalQuestion = goalLayout.findViewById<ImageView>(R.id.preferenceQuestion)
+
+        val rateoLayout = view.findViewById<LinearLayout>(R.id.rateo)
+        val rateoName = rateoLayout.findViewById<TextView>(R.id.preferenceName)
+        rateoName.text = getString(R.string.rateo_label)
+        val rateoValue = rateoLayout.findViewById<EditText>(R.id.preferenceValue)
+        rateoValue.setText(rateo)
+        //val rateoQuestion = rateoLayout.findViewById<ImageView>(R.id.preferenceQuestion)
+
+        val sensLayout = view.findViewById<LinearLayout>(R.id.sensitivity)
+        val sensName = sensLayout.findViewById<TextView>(R.id.preferenceName)
+        sensName.text = getString(R.string.sensibility_label)
+        val sensValue = sensLayout.findViewById<EditText>(R.id.preferenceValue)
+        sensValue.setText(sens)
+        //val sensQuestion = sensLayout.findViewById<ImageView>(R.id.preferenceQuestion)
+
+        val btnSave = view.findViewById<TextView>(R.id.btnSavePreference)
+        btnSave.setOnClickListener {
+            daily = dailyValue.text.toString()
+            goal = goalValue.text.toString()
+            rateo = rateoValue.text.toString()
+            sens = sensValue.text.toString()
+
+            val editor = sharedPref?.edit()
+
+            editor?.putString(DAILY, daily)
+            editor?.putString(GOAL, goal)
+            editor?.putString(RATEO, rateo)
+            editor?.putString(SENS, sens)
+
+            editor?.apply()
+            Toast.makeText(
+                requireContext(),
+                "Dati salvati",
+                Toast.LENGTH_SHORT
+            ).show()
+
+        }
 
         val btnTheme = view.findViewById<TextView>(R.id.btnTheme)
         btnTheme.setOnClickListener {
             selectThemeColor()
         }
+
 
         return view
     }
@@ -78,12 +142,9 @@ class SettingsFragment : Fragment() {
         val editor = sharedPref?.edit()
         editor?.putString(THEME, chosenColor)
         editor?.apply()
+        (activity as MainActivity?)?.switchTheme()
+        findNavController().navigate(R.id.settingsFragment)
         themeDialog.dismiss()
-        Toast.makeText(
-            requireContext(),
-            "Riavvia l'app per apportare la modifica",
-            Toast.LENGTH_SHORT
-        ).show()
 
     }
 
